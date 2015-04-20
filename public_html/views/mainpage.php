a7a884fbc3c9873f64f82e1bc25d8da1f365a30c
Alfred Cepeda <AlfredJCepeda@gmail.com>
2015-04-20 02:57:11 -0700

    Completes main page
    Fixes routing by using fallback instead of rewrite
    Removed front-end dependencies from composer and migrated to bower

--- public_html/views/mainpage.php
+++ public_html/views/mainpage.php
@@ -1,5 +1,65 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="icon" href="/assets/images/icons/favicon.ico">
 
-echo "This is from the main page!";
+	<title>Main Page</title>
 
-?>
+	<!-- Bootstrap core CSS -->
+	<link href="/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Custom styles for this template -->
+	<link href="/assets/css/bootstrap/cover.css" rel="stylesheet">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+	<div class="alert alert-danger alert-dismissible" role="alert">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+		</button>
+	  	<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+	  	<span class="sr-only">Warning:</span>
+	  	This site is currently under construction.
+	</div>
+    <div class="site-wrapper">
+      	<div class="site-wrapper-inner">
+        	<div class="cover-container">
+          		<div class="masthead clearfix">
+            		<div class="inner">
+	              		<h3 class="masthead-brand">ALJCepeda</h3>
+		              	<nav>
+		                	<ul class="nav masthead-nav">
+		                  		<li class="active"><a href="#">Home</a></li>
+		                	</ul>
+		              	</nav>
+            		</div>
+         		</div>
+
+		        <div class="inner cover">
+		           	<h1 class="cover-heading">Welcome</h1>
+		            <p class="lead">A look into the world of a man who thinks too much</p>
+		        </div>
+        	</div>
+    	</div>
+    </div>
+</body>
+
+<footer class="footer">
+    
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+</html>
