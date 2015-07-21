e96d39e142b4a3376b0f36e01722d5695a857726
Alfred Cepeda <AlfredJCepeda@gmail.com>
2015-07-21 00:44:50 -0700

    Adds description about chat app

--- public/views/chat.php
+++ public/views/chat.php
@@ -6,5 +6,12 @@ if(ISLOCAL){
 }
 
 ?>
- 
-<object type="text/html" data="<?=$chatURL?>" width="400" height="450" />
+
+<h1>Live Chat</h1>
+
+<object type="text/html" data="<?=$chatURL?>" width="400" height="450">
+</object>
+
+<p>
+A simple lightweight application written with NodeJS and Socket.io. It implements CSS3 flexbox which will make it fully reactive to small UIs. This client will eventually be adopted into a taskbar application similar to facebook's messenger.
+</p>
