d6a6238d0c161f447b94a09e8338172a010d7bba
Alfred Cepeda <AlfredJCepeda@gmail.com>
2015-12-30 19:53:40 +0000

    Restores local environment

--- config.php
+++ config.php
@@ -1,8 +1,9 @@
 <?php
-require 'constants.php';
+require "constants.php";
+require "aljcepeda.php";
 
-require 'misc.php';
-require VENDOR . '/autoload.php';
-require 'resources/security/errorhandler.php';
+require "misc.php";
+require VENDOR . "/autoload.php";
+require "resources/security/errorhandler.php";
 
 date_default_timezone_set(TIMEZONE);
