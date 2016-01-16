8e8ab28daf60121dc0a1ae110643aaf1d81b3de0
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-01-16 06:10:33 +0000

    Adds portfolio page

--- resources/router/routes.php
+++ resources/router/routes.php
@@ -3,43 +3,45 @@ use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
 
 //This is being initialized here just for convenience.
-//Determines what's displayed in navbar.php
+//Determines what"s displayed in navbar.php
 $menuList = [
-				'Home' => '/',
-				'Eval' => '/eval',
-				'Snake' => '/snake',
-			 	'Repair' => '/repair',
-			 	'About Me' => '/aboutme'
+				"Home" => "/",
+				"Eval" => "/eval",
+				"Snake" => "/snake",
+			 	"Repair" => "/repair",
+			 	"Portfolio" => "/portfolio",
+			 	"About Me" => "/aboutme"
 			];
 
 $routes = new RouteCollection();
-$routes->add('mainpage', new Route('/'));
-$routes->add('redirect', new Route('/redirect'));
-$routes->add('404', new Route('/error/404', ['title' => '404 Error', 'script' => 'views/error/404.html']));
-$routes->add('invalidsession', new Route('/error/invalid', ['title' => 'Invalid Request', 'script' => 'views/error/invalidsession.php', 'error' => 'badsession']));
-$routes->add('repair', new Route('/repair', ['title' => 'Repair']));
-$routes->add('aboutme', new Route('/aboutme', [ 'title' => 'About Me' ]));
-$routes->add('eval', new Route('/eval', 
+$routes->add("mainpage", new Route("/"));
+$routes->add("redirect", new Route("/redirect"));
+$routes->add("404", new Route("/error/404", ["title" => "404 Error", "script" => "views/error/404.html"]));
+$routes->add("invalidsession", new Route("/error/invalid", ["title" => "Invalid Request", "script" => "views/error/invalidsession.php", "error" => "badsession"]));
+$routes->add("repair", new Route("/repair", ["title" => "Repair"]));
+$routes->add("aboutme", new Route("/aboutme", [ "title" => "About Me" ]));
+$routes->add("eval", new Route("/eval", 
 									[ 
-										'title' => 'Eval', 
-										'css' => [ 'buttons', 'colors' ]
+										"title" => "Eval", 
+										"css" => [ "buttons", "colors" ]
 									]
 								));
-$routes->add('snake', new Route('/snake', 
+$routes->add("snake", new Route("/snake", 
 									[
-										'title' => 'Snake.IO',
-										'css' => [ 'buttons', 'colors' ],
-										'notification' => [
-											'status' => 'Down',
-											'type' => 'alert-danger',
-											'dismissable' => true,
-											'message' => 'Snake.IO has been taken down until I can afford a better web host'
+										"title" => "Snake.IO",
+										"css" => [ "buttons", "colors" ],
+										"notification" => [
+											"status" => "Down",
+											"type" => "alert-danger",
+											"dismissable" => true,
+											"message" => "Snake.IO has been taken down until I can afford a better web host"
 										]
 									]
 								));
-$routes->add('createUser', new Route('/user/create', 
+$routes->add("portfolio", new Route("/portfolio", [ "title" => "Portfolio" ]));
+$routes->add("createUser", new Route("/user/create", 
 									[
-										'title' => 'Create User',
-										'require' => ['recaptcha']
+										"title" => "Create User",
+										"require" => ["recaptcha"]
 									]
 								));
