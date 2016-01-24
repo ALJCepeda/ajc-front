bcbbd83d4d0e21935cd2378ea50534cd36853283
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-01-24 00:26:37 +0000

    Imroves styling

--- public/views/gtk.php
+++ public/views/gtk.php
@@ -1,3 +1,22 @@
+<?php
+	class TidBit {
+		public $description = "";
+		public $command = "";
+
+		function __construct($command, $description) {
+			$this->description = $description;
+			$this->command = $command;
+		}
+	}
+
+	$tidbits = [
+		new TidBit( "find . -type d ! -perm 755 -exec chmod -v 755 {} \; && </br> find . -type f ! -perm 644 -exec chmod -v 644 {} \;",
+					"Changes permissions of all files to 644 and all folders to 755 in (and including) the current directory"),
+		new TidBit( "sudo lsof -i :80",
+					"Returns the pids of processes bound to port 80")
+	];
+?>
+
 <style type="text/css">
 	.header {
 		flex: 2;
@@ -10,6 +29,19 @@
 	.main {
 		flex: 8;
 	}
+	.gutter {
+		min-width:20px;
+	}
+	.row ~ .row {
+		margin-top:20px;
+	}
+
+	.row:nth-child(even) {
+		color:silver;
+	}
+	.row:nth-child(odd) {
+		color:lightblue;
+	}
 </style>
 
 <div class="q4 row-nw m-center">
@@ -20,28 +52,38 @@
 
 		<div class="intro q3-w text-center">
 			<p>
-				This page contains usefull little tidbits (mainly bash commands) that I've found and can never seem to remember.
+				This page contains useful little tidbits (mainly bash commands) that I've found and can never seem to remember.
 
-				I share them with you! And also because I can't be bothered dealing with Google Drive sometimes.
+				I share them with you! And also because I can't be bothered logging into Google Drive.
 			</p>
 		</div>
 
-		<div class="main row-nw wide">
-			<div class="aside left">
-				<h3>Description</h3>
+		<div class="main wide">
+			<div class="row-nw">
+				<div class="aside">
+					<h3>Commands</h3>
+				</div>
 
-				<div class="row">
-					1. Changes all file permissions to 644 and all folder permissions to 755 in current directory
+				<div class="aside">
+					<h3>Descriptions</h3>
 				</div>
 			</div>
 
-			<div class="aside right">
-				<h3>Command</h3>
+			<?php foreach ($tidbits as $index => $tid) {	?>
+				<div class="row row-nw">
+					<div class="gutter">
+						<?= $index+1 ."." ?>
+					</div>
 
-				<div class="row">
-					1. find . -mindepth 1 -type d ! -perm 755 -exec chmod -v 755 {} \; &&  find . -mindepth 1 -type f ! -perm 644 -exec chmod -v 644 {} \;
+					<div class="aside">
+						<?= $tid->command ?>
+					</div>
+
+					<div class="aside">
+						<?= $tid->description ?>
+					</div>
 				</div>
-			</div>
+			<?php }	?>
 		</div>
 	</div>
 </div>
