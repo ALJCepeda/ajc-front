8968946d40684e9a543fe2afbf57a0f09e3b6a14
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-08-21 23:58:01 -0400

    Active menu item is an oberservable

--- public/index.js
+++ public/index.js
@@ -9,20 +9,22 @@ define([], function() {
             'Portfolio',
             'About Me'
         ];
-        this.activeMenuItem;
+        this.activeMenuItem = ko.observable();
 
         this.didClickMenu = function(item, e) {
             var elem = e.currentTarget;
             elem.className += 'active';
 
-            self.activeMenuItem.className = '';
-            self.activeMenuItem = elem;
+            self.activeMenuItem().className = '';
+            self.activeMenuItem(elem);
             console.log('Did Click: ' + item);
         }
     };
 
     var vm = new mainVM();
 	ko.applyBindings(vm, document.getElementById('main'));
-    vm.activeMenuItem = document.getElementById('menu_Home');
-    vm.activeMenuItem.className += 'active';
+
+    var home = document.getElementById('menu_Home')
+    vm.activeMenuItem(home);
+    home.className += 'active';
 });
