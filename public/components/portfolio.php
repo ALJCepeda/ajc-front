b43535a2eb336a1ae7ce794e369249f41929ac54
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-08-24 03:57:18 -0400

    Moves left side over 40 pixels

--- public/components/portfolio.php
+++ public/components/portfolio.php
@@ -11,14 +11,17 @@ $entries = generate_entries();
 </div>
 
 <div class='container-fluid'>
-	<div class='row row-nw m-start c-center'>
+	<div class='row-nw m-center c-center'>
+		<h3>Projections</h3>
+	</div>
+	<div class='row-nw m-start c-center'>
 		<div class='col-xs-2 menu'>
 			<ul>
-				<li>Bar</li>
-				<li>Chart</li>
-				<li>Grouped</li>
-				<li>Expanded</li>
-				<li>Both</li>
+				<li style='padding-left:10px'>Bar</li>
+				<li style='padding-left:10px'>Chart</li>
+				<li style='padding-left:10px'>Grouped</li>
+				<li style='padding-left:10px'>Expanded</li>
+				<li style='padding-left:10px'>Both</li>
 			</ul>
 		</div>
 		<div class='col-xs-5'>
@@ -32,7 +35,7 @@ $entries = generate_entries();
 						DevExpress was used for the bar and area graphs. KnockoutJS for UI two-way binding and responsiveness. Bootstrap for layout and CSS styling. Behind the scenes I was forced to make clever use of the Adapter and Delegate design patterns in order to handle some poor decisions made by my senior developer
 					</div>
 				</li>
-				<li class='active'>
+				<li>
 					What's the point of this app?
 					<div>
 						TerrorTorch's main attraction was the ability to frighten people as they passed by camera by playing a sudden loud sound. The camera would record their reactions and the video would be uploaded to a server to be viewed in a public gallery. This was a project being hosted by the domain http://reBaked.com
