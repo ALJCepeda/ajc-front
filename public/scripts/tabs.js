d83decca20d5c002f06aa11045fe0261b57944b7
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-10-02 15:21:00 -0400

    Renames some files
    These are pages, not components

--- public/scripts/tabs.js
+++ public/scripts/tabs.js
@@ -1,11 +1,11 @@
 define([], function() {
     return [
-        { id:'home', name:'Home', hash:'#home', url:'/components/home.php' },
-        { id:'blog', name:'Blog', hash:'#blog', url:'/components/blog.php', js:'/components/blog.js' },
-        { id:'eval', name:'Eval', hash:'#eval', url:'/components/eval.php' },
-        { id:'repair', name:'Repair', hash:'#repair', url:'/components/repair.php' },
-        { id:'portfolio', name:'Portfolio', hash:'#portfolio', url:'/components/portfolio.php', js:'/components/portfolio.js' },
-        { id:'aboutme', name:'About Me', hash:'#aboutme', url:'/components/aboutme.php' },
-        { id:'invalid', hash:'#invalid', url:'/components/invalid.php' }
+        { id:'home', name:'Home', hash:'#home', url:'/pages/home.php' },
+        { id:'blog', name:'Blog', hash:'#blog', url:'/pages/blog.php', js:'/pages/blog.js' },
+        { id:'eval', name:'Eval', hash:'#eval', url:'/pages/eval.php' },
+        { id:'repair', name:'Repair', hash:'#repair', url:'/pages/repair.php' },
+        { id:'portfolio', name:'Portfolio', hash:'#portfolio', url:'/pages/portfolio.php', js:'/pages/portfolio.js' },
+        { id:'aboutme', name:'About Me', hash:'#aboutme', url:'/pages/aboutme.php' },
+        { id:'invalid', hash:'#invalid', url:'/pages/invalid.php' }
     ];
 });
