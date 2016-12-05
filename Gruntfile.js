6a37448f32fb20091273779aa8dca0abab23b5bb
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-12-05 18:16:23 -0500

    Commit before reboot

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,26 @@
 module.exports = function(grunt) {
+  var lessFiles = [
+    'less/index.less'
+  ];
 
+  grunt.initConfig({
+    concat: {
+      default: {
+        src: lessFiles,
+        dest: 'less/built.less'
+      }
+    },
+    less: {
+      default: {
+        files: {
+          'public/assets/css/styles.css':'less/built.less'
+        }
+      }
+    }
+  });
+
+grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  grunt.registerTask('default', ['concat', 'less']);
 };
