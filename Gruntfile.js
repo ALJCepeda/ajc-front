c120a6d8b07bb6abe867356bdcc3ac1d7af66ae9
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-12-21 22:54:56 -0500

    Watching files, adds less files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,10 @@
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
   var lessFiles = [
+    'less/variables.less',
     'less/index.less'
   ];
 
@@ -16,11 +21,25 @@ module.exports = function(grunt) {
           'public/assets/css/styles.css':'less/built.less'
         }
       }
+    },
+    watch: {
+      default: {
+        files:lessFiles,
+        tasks: ['concat', 'less']
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-less');
+  let changed = [];
+  var onChange = grunt.util._.debounce(function() {
+    grunt.config('concat.default.src', changed);
+    changed = [];
+  }, 200);
 
-  grunt.registerTask('default', ['concat', 'less']);
+  grunt.event.on('watch', function(action, filepath) {
+    changed.push(filepath);
+    onChange();
+  });
+
+  grunt.registerTask('default', ['concat', 'less', 'watch']);
 };
