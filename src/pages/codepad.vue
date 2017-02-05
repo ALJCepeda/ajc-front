4a0a1a8c1a08b0e97dc106a97c5033a7cac895f8
Alfred Cepeda <AlfredJCepeda@gmail.com>
2017-02-05 00:37:43 -0500

    Add project card an eval information

--- src/pages/codepad.vue
+++ src/pages/codepad.vue
@@ -1,62 +1,24 @@
 <template>
-  <div class='page'>
-    <div class='page-content'>
-    	<div class='well col-xs-10'>
-    		<div class='flex-container'>
-    			<div class='header'>
-    				<h1>Eval</h1>
-    			</div>
-
-    			<div class='left'>
-    				<h1>About</h1>
-    				<p>Multi-language multi-version code intepreter/compiler</p>
-
-    				<br />
-    				<p>General purpose, single page code editor for testing programming logic on the fly</p>
-
-    				<br />
-
-    				<div>
-    					<h1>Technologies: </h1>
-    					<ul>
-    						<li><a href='https://nodejs.org/en/'>NodeJS</a> - Webserver</li>
-    						<li><a href='https://ace.c9.io/#nav=about'>Ace-builds</a> - Code Editor</li>
-    						<li><a href='http://knockoutjs.com'>Knockoutjs</a> - Two-way Bind/Observable</li>
-    						<li><a href='https://www.docker.com'>Docker</a> - Virtualization</li>
-    						<li><a href='https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Using_CSS_flexible_boxes'>Flexbox</a> - Layout</li>
-    					</ul>
-    				</div>
-    			</div>
-
-    			<div class='right'>
-    				<div>
-    					<h1>To Do:</h1>
-    					<ul>
-    						<li>Multi-version support</li>
-    						<li>Multi-document support</li>
-    						<li>SQL support</li>
-    						<li>Webserver support</li>
-    						<li>Cross-platform support</li>
-    					</ul>
-    				</div>
-    			</div>
-
-    			<div class='footer'>
-    				<a href='<?= $evalURL ?>'>
-    			   		<input class='orange button' type='button' value='Code Now!' />
-    				</a>
-
-    				<p>Version: <?= $version ?> </p>
-    			</div>
-    		</div>
-    	</div>
-    </div>
+  <div>
+    <project-card v-bind:data='eval'></project-card>
   </div>
 </template>
 
 <script>
+  import projectCard from './../components/project-card.vue';
+
   export default {
-    name: 'codepad'
+    name: 'codepad',
+    components: { projectCard },
+    data: function() {
+      return {
+        eval: {
+          title: 'Eval',
+          description: 'This is a test',
+          goto: 'http://google.com'
+        }
+      };
+    }
   };
 </script>
 
