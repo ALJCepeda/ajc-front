95197f6de33341f6bd3eb75c734b63960cdd890c
Alfred Cepeda <AlfredJCepeda@gmail.com>
2017-02-05 12:58:09 -0500

    Good looking layout to top panel

--- src/components/main-nav.vue
+++ src/components/main-nav.vue
@@ -2,18 +2,20 @@
   <div class='container'>
     <div class='cover-photo'></div>
     <div class='header'>
-      <div class='main-photo'></div>
+      <div class='main-photo'>
+        <img src='./../assets/images/me.jpeg'></img>
+      </div>
       <div class='content'>
         <div class='info'>
           <span>Alfred Cepeda</span>
         </div>
 
-        <nav>
-          <ul>
+        <div class='nav'>
+          <!--<ul>
             <li>Timeline</li>
             <li>About</li>
-          </ul>
-        </nav>
+          </ul>-->
+        </div>
       </div>
     </div>
   </div>
@@ -31,12 +33,15 @@
   .container {
     width:100%;
     height:70%;
-    background-color:blue;
+    background:#fff;
   }
 
   .cover-photo {
     height:85%;
-    background-color:cyan;
+    background-image:url('../assets/images/workstation.png');
+    background-size:cover;
+    background-repeat:no-repeat;
+    background-position:center center;
   }
 
   .header {
@@ -45,44 +50,51 @@
     position:relative;
     top:-130px;
 
-    background-color:green;
     width:100%;
-    height:185px;
+    height:46%;
   }
 
   .main-photo {
-    height:100%;
-    width:25%;
-    min-height:150px;
-    min-width:150px;
+    .row-nw;.c-center;.m-center;
 
-    background-color:yellow;
+    height:100%;
+    width:20%;
+
+    img {
+      height:155px;
+      width:170px;
+
+      border:4px solid #fff;
+      border-radius:2px;
+    }
   }
 
   .content {
-    background-color:purple;
-    width:75%;
+    width:80%;
     height:100%;
 
     .info {
       position:relative;
-      background-color:brown;
-      height:75%;
+      height:68%;
 
       span {
         position:absolute;
-        bottom:0;
-        left:0;
+        bottom:10px;
+        left:5px;
+
+        color:#fff;
+        font-family:Helvetica, Arial, sans-serif;
+        font-size:24px;
+        font-weight:500;
+        line-height:30px;
       }
     }
 
-    nav {
-      height:25%;
+    .nav {
+      height:32%;
       width:100%;
-      
-      ul {
-        background:blue;
-      }
+
+      background:blue;
     }
   }
 </style>
