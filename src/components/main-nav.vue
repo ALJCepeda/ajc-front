f82ee58c95300e0b189ac20cb026fc153a9df15f
Alfred Cepeda <AlfredJCepeda@gmail.com>
2017-02-05 01:44:30 -0500

    Slightly better layout

--- src/components/main-nav.vue
+++ src/components/main-nav.vue
@@ -1,8 +1,21 @@
 <template>
   <div class='container'>
     <div class='cover-photo'></div>
-    <nav></nav>
-    <div class='main-photo'></div>
+    <div class='header'>
+      <div class='main-photo'></div>
+      <div class='content'>
+        <div class='info'>
+          <span>Alfred Cepeda</span>
+        </div>
+
+        <nav>
+          <ul>
+            <li>Timeline</li>
+            <li>About</li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,7 +25,9 @@
   };
 </script>
 
-<style scoped>
+<style lang='less' scoped>
+  @import './../less/flex.less';
+
   .container {
     width:100%;
     height:70%;
@@ -24,20 +39,50 @@
     background-color:cyan;
   }
 
-  .main-photo {
-    position:relative;
-    top:-190px;
-    left:20px;
+  .header {
+    .row-nw;
 
-    height:47%;
-    width:17%;
+    position:relative;
+    top:-130px;
+
+    background-color:green;
+    width:100%;
+    height:185px;
+  }
+
+  .main-photo {
+    height:100%;
+    width:25%;
     min-height:150px;
     min-width:150px;
-    
+
     background-color:yellow;
   }
 
-  nav {
-    background-color:green;
+  .content {
+    background-color:purple;
+    width:75%;
+    height:100%;
+
+    .info {
+      position:relative;
+      background-color:brown;
+      height:75%;
+
+      span {
+        position:absolute;
+        bottom:0;
+        left:0;
+      }
+    }
+
+    nav {
+      height:25%;
+      width:100%;
+      
+      ul {
+        background:blue;
+      }
+    }
   }
 </style>
--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import $ from 'jquery';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.js';
-import 'materialize-css/dist/css/materialize.css';
-import 'materialize-css/dist/js/materialize.js';
 import './less/flex.less';
 import './less/variables.less';
 import './less/index.less';
